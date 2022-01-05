c2931fe4aefe3c2250f91c9d9223e9b3c681b40c
JoseNeto09 <joselopessobrinhoneto9@gmail.com>
2022-01-04 21:10:04 -0300

    databind

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,14 +4,12 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [
-    HomeComponent,
-    LoginComponent
-  ],
-  imports: [CommonModule,HomeRoutingModule],
+  declarations:[HomeComponent, LoginComponent],
+  imports: [CommonModule, HomeRoutingModule, FormsModule],
   exports: [HomeComponent],
 })
 export class HomeModule { }
