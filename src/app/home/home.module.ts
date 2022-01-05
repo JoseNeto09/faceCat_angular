a5e09c599d7cdae444ab54976d2219823c1eb206
JoseNeto09 <joselopessobrinhoneto9@gmail.com>
2022-01-05 17:01:55 -0300

    validação de campos e formularios

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,11 +5,12 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
+import { MensagemModule } from '../componentes/mensagem/mensagem.module';
 
 
 @NgModule({
   declarations:[HomeComponent, LoginComponent],
-  imports: [CommonModule, HomeRoutingModule, FormsModule],
+  imports: [CommonModule, HomeRoutingModule, FormsModule, MensagemModule],
   exports: [HomeComponent],
 })
 export class HomeModule { }
