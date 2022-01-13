f55a0ef56cd36668d7ebc91a86de61e0f2d7a760
JoseNeto09 <joselopessobrinhoneto9@gmail.com>
2022-01-13 18:16:51 -0300

    tela de formulario feita

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -2,6 +2,7 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,11 @@ const routes: Routes = [
         path: '',
         component:LoginComponent,
       },
+      {
+        path: "novousuario",
+        component: NovoUsuarioComponent,
+
+      }
     ],
   },
 ];
